d9439557cc43f7b5508bdd4658fee8cc65a11377
tmarkj <tmarkj@mit.edu>
2023-09-20 10:39:48 -0400

    added better docstring

--- inflammation/models.py
+++ inflammation/models.py
@@ -19,16 +19,25 @@ def load_csv(filename):
 
 
 def daily_mean(data):
-    """Calculate the daily mean of a 2D inflammation data array."""
+    """Calculate the daily mean of a 2D inflammation data array.
+    :param data: a numpy array containing inflammation data 
+    :returns: a numpy array of daily mean
+    """
     return np.mean(data, axis=0)
 
 
 def daily_max(data):
-    """Calculate the daily max of a 2D inflammation data array."""
+    """Calculate the daily max of a 2D inflammation data array.
+    :param data: a numpy array containing inflammation data 
+    :returns: a numpy array of daily max
+    """
     return np.max(data, axis=0)
 
 
 def daily_min(data):
-    """Calculate the daily min of a 2D inflammation data array."""
+    """Calculate the daily min of a 2D inflammation data array.
+    :param data: a numpy array containing inflammation data 
+    :returns: a numpy array of daily min
+    """
     return np.min(data, axis=0)
 
